69e25ebe3f46ed358c94fa781f9792053100e23d
frc4533-lincoln <132951735+frc4533-lincoln@users.noreply.github.com>
2023-09-06 14:22:22 +0000

    Update package.sh

--- package.sh
+++ package.sh
@@ -20,15 +20,14 @@ done
 
 # Create dist directory structure
 mkdir -p ./wpilib-novsc-athena/usr/lib/ ./wpilib-novsc-athena/usr/include/ \
-  ./wpilib-novsc-toolchain/usr/bin/ ./wpilib-novsc-toolchain/usr/libexec/ \
-  ./wpilib-novsc-sysroot/ \
+  ./wpilib-novsc-toolchain/usr/bin/ ./wpilib-novsc-toolchain/usr/libexec/wpilib-novsc/gcc/bin/ ./wpilib-novsc-toolchain/usr/libexec/wpilib-novsc/gcc/libexec/ ./wpilib-novsc-toolchain/usr/libexec/wpilib-novsc/roborio/bin/ ./wpilib-novsc-toolchain/usr/libexec/wpilib-novsc/roborio/lib/ \
+  ./wpilib-novsc-sysroot/usr/libexec/wpilib-novsc/roborio/sysroot/ ./ \
   ./wpilib-novsc-docs/cpp/ ./wpilib-novsc-docs/frc/
 
 cd ./wpilib-novsc-tmp/
 
 # Rename NI libs
 # Move libraries into dist
-mv ./linux/athena/shared/libRoboRIO_FRC_ChipObject.so.* ./linux/athena/shared/libRoboRIO_FRC_ChipObject.so
 mv ./linux/athena/shared/libvisa.so.* ./linux/athena/shared/libvisa.so
 mv ./linux/athena/shared/libFRC_NetworkCommunication.so.* ./linux/athena/shared/libFRC_NetworkCommunication.so
 mv ./linux/athena/shared/*.so ../wpilib-novsc-athena/usr/lib/
@@ -38,29 +37,37 @@ mkdir ./frc-headers/
 mv ./frc/ ./frc2/ ./wpi/ ./wpinet/ ./wpimath/ ./networktables/ \
   ./units/ ./unsupported/ ./uv/ ./gcem_incl/ ./drake/ ./Eigen/ \
   ./hal/ ./fmt/ ./gcem.hpp ./uv.h ./WPILibVersion.h ./ntcore* \
-  ./FRC_NetworkCommunication/ ./visa/ ./frc-headers/
+  ./cscore* ./FRC_NetworkCommunication/ ./visa/ ./frc-headers/
 mv ./frc-headers/ ../wpilib-novsc-athena/usr/include/frc/
 
 # Move documentation into dist
 mv ./documentation/cpp/* ../wpilib-novsc-docs/cpp/
 mv ./documentation/rtd/* ../wpilib-novsc-docs/frc/
 
-# Move roborio toolchain into dist
-mv ./roborio/bin/* ../wpilib-novsc-toolchain/usr/bin/
-mv ./roborio/libexec ../wpilib-novsc-toolchain/usr/libexec/
+# Generate wrappers for each gcc binary
+# Move wrappers and binaries into dist
+for f in ./roborio/bin/*; do
+  # Move vanilla binary into dist
+  mv ${f} ../wpilib-novsc-toolchain/usr/libexec/wpilib-novsc/gcc/bin/
+  # Generate a wrapper and write to dist
+  printf '#!/bin/sh\n\nCOMPILER_PATH="$(pwd)/../libexec/wpilib-novsc/roborio/bin" ' > "../wpilib-novsc-toolchain/usr/bin/${f##./roborio/bin/}"
+  printf "%s/../libexec/wpilib-novsc/gcc/bin/${f##./roborio/bin/}\n" '$(pwd)' >> "../wpilib-novsc-toolchain/usr/bin/${f##./roborio/bin/}"
+done
+mv ./roborio/libexec/* ../wpilib-novsc-toolchain/usr/libexec/wpilib-novsc/gcc/libexec/
+mv ./roborio/arm-nilrt-linux-gnueabi/bin/* ../wpilib-novsc-toolchain/usr/libexec/wpilib-novsc/roborio/bin/
+mv ./roborio/arm-nilrt-linux-gnueabi/lib/* ../wpilib-novsc-toolchain/usr/libexec/wpilib-novsc/roborio/lib/
 
-# Remove debug info and source code from roborio sysroot
+# Remove debug info from roborio sysroot
 # Move roborio sysroot into dist
 rm -r ./roborio/arm-nilrt-linux-gnueabi/sysroot/lib/.debug/ \
-  ./roborio/arm-nilrt-linux-gnueabi/sysroot/usr/lib/.debug/ \
-  ./roborio/arm-nilrt-linux-gnueabi/sysroot/usr/src/
-mv ./roborio/arm-nilrt-linux-gnueabi/sysroot/* ../wpilib-novsc-sysroot/
+  ./roborio/arm-nilrt-linux-gnueabi/sysroot/usr/lib/.debug/
+mv ./roborio/arm-nilrt-linux-gnueabi/sysroot/* ../wpilib-novsc-sysroot/usr/libexec/wpilib-novsc/roborio/sysroot/
 
 cd ../
 
 # Pack and compress dist tarballs
 for distdir in "wpilib-novsc-athena" "wpilib-novsc-toolchain" "wpilib-novsc-sysroot" "wpilib-novsc-docs"; do
-  cat ./wpilib-novsc-tmp/LICENSE.md ./wpilib-novsc-tmp/ThirdPartyNotices.txt > ./${distdir}/LICENSE.txt
+  cat ./LICENSE.md ./ThirdPartyNotices.txt > ./${distdir}/LICENSE.txt
   tar -C ${distdir} -cvf "${distdir}.tar" .
   xz -9e "${distdir}.tar"
 done
